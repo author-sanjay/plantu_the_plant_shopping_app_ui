5b8836386d81f786a0b34100eb474940d8a5fc57
Sanjay Kumar <sanjaykumar73189@gmail.com>
2022-01-14 18:27:03 +0530

    added search bar

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:plant_app/constants.dart';
 class Body extends StatelessWidget {
   const Body({ Key? key }) : super(key: key);
@@ -18,8 +19,43 @@ class Body extends StatelessWidget {
             bottomRight: Radius.circular(36)
           ),
         ),
+        
         ),
-      ],
+      Positioned(
+        bottom: 0,
+        left: 0,
+        right: 0,
+        child: Container(
+          alignment: Alignment.center,
+          margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+          padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+          height: 54,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              offset: Offset(0,10),
+              blurRadius: 50,
+              color: kPrimaryColor.withOpacity(0.23),
+            )
+          ]
+        ),
+        child: Row(
+          children: <Widget>[TextField(
+            decoration: InputDecoration(
+              hintText: "Search",
+              hintStyle: TextStyle(color: kPrimaryColor.withOpacity(0.5),),
+              enabledBorder: InputBorder.none,
+              focusedBorder: InputBorder.none,
+              //suffixIcon: SvgPicture.asset("assets/icons/search.svg"),
+            ),
+          ),
+          ]
+        ),
+        )
+      )
+      ],  
     );
   }
 }
